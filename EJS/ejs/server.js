7c11990c45878dd5d07f026706d94fcef7f2770f
hackspark <hackspark@outlook.com>
2024-09-25 12:09:03 +0000

    working on notes

--- EJS/ejs/server.js
+++ EJS/ejs/server.js
@@ -10,6 +10,15 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/random', (req, res) => {
+  const num = Math.floor(Math.random() * 10)
+  res.render('random', {rand: num})
+})
+
+app.get('/r/:subreddit', (req, res) => {
+  const {subreddit} = req.params
+  res.render('subreddit', { subreddit })
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!")
