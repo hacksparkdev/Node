061caa921dcad4d99a3063fc59ce23c9dc092b7c
hackspark <hackspark@outlook.com>
2024-09-28 22:21:10 +0000

    Updated notes

--- elasticsearch/server.js
+++ elasticsearch/server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.set('view', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'))
 
 
 const client = new Client({
