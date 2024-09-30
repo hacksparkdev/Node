e46a60efc35f0a9a1ca3de9e3916a5603ce4a71b
hackspark <hackspark@outlook.com>
2024-09-30 20:33:48 +0000

    Updated notes

--- resftulRoutes/server.js
+++ resftulRoutes/server.js
@@ -1,13 +1,15 @@
-const express = require('expres');
+const express = require('express');
 const app = express();
 
+app.use(express.json()) // Request that come in are parsed as json 
 
 app.get('/tacos', (req,res) => {
   res.send("GET /tacos response")
 })
 
 app.post('/tacos', (req, res) => {
-  res.send("POST /tacos response")
+  const {meat, qty } = req.body; 
+  res.send(`Here is your ${qty} ${meat} tacos `)
 })
 
 app.listen(3000, () => {
